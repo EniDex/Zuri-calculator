879634c00c616a9f327d3348f32f23ff0607cefd
Obirije Eniola Destiny <obirijedestiny@gmail.com>
2022-06-24 15:21:00 +0100

    new commit

---
+++ home.js
@@ -0,0 +1,15 @@
+
+
+let x,y;
+
+x = parseInt(prompt("enter the first number"))
+y = parseInt(prompt("enter the second number"))
+let z=x+y;
+let a=x-y;
+let b=x*y;
+let c=x/y;
+
+alert("the sum of given number is:"+z);
+alert("the difference of given number is:"+a);
+alert("the product of given number is:"+b);
+alert("the quotient of given number is:"+c)
